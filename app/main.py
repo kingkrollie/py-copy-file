c894874005f7744e5df05989d3c6b46e1abb3974
Vadym Vynokur <king.krollie@gmail.com>
2025-11-06 13:39:50 +0200

    done

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 def copy_file(command: str) -> None:
     command = command.split()
+
     if len(command) != 3 or command[0] != "cp":
         return
+    source = command[1]
+    target = command[2]
 
-    if command[1] == command[2]:
+    if source == target:
         return
     try:
-        with (open(command[1], "r") as file_in,
-              open(command[2], "w") as file_out):
+        with (open(source, "r") as file_in,
+              open(target, "w") as file_out):
             for line in file_in:
                 file_out.write(line)
     except FileNotFoundError:
